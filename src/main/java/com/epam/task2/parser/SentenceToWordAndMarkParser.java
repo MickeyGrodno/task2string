0f43f59916664eb7000361cac6bc532c11dca393
Sergei <sergei199>
2021-03-29 16:31:12 +0300

    Исправлена ошибка с деффисом в словах

--- src/main/java/com/epam/task2/parser/SentenceToWordAndMarkParser.java
+++ src/main/java/com/epam/task2/parser/SentenceToWordAndMarkParser.java
@@ -9,7 +9,7 @@ public class SentenceToWordAndMarkParser {
     private final String WORD_ELEMENTS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvw" +
             "xyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'\\";
     private final String END_OF_SENTENCE_MARK = ".!?…";
-    private final String PUNCTUATION_MARK = "<>:;\'\"@#№$%^&*)({}][—=+/~,|-";
+    private final String PUNCTUATION_MARK = "<>:;\'\"@#№$%^&*)({}][—=+/~,|-•";
     Set wordElementsSet = new HashSet();
 
 
@@ -21,51 +21,38 @@ public class SentenceToWordAndMarkParser {
         sentence.addComponentToList(new PunctuationMark(String.valueOf(mark)));
     }
 
-
-    public CompositeStringPart parseSentenсeToWordAndMark(String sentenceInString) {
+    public Sentence parseSentenсeToWordAndMark(String sentenceInString) {
         StringBuilder word = new StringBuilder();
-        CompositeStringPart sentence = new Sentence();
-        char previousItem = '0';
+        Sentence sentence = new Sentence();
+        char previousElement = '0';
         int counter = 0;
         for (char element : WORD_ELEMENTS.toCharArray()) {
             wordElementsSet.add(element);
         }
-
-
         char[] sentenceInChar = sentenceInString.toCharArray();
         for (char element : sentenceInChar) {
             counter++;
-            if (wordElementsSet.contains(element) || (element == '-' && wordElementsSet.contains(previousItem))) {
+            if (wordElementsSet.contains(element) || (element == '-' && wordElementsSet.contains(previousElement))) {
                 word.append(element);
-                continue;
-            }
-            if (element == ' ' && word.length() != 0) {
+            } else if (element == ' ' && word.length() != 0) {
                 saveWord(sentence, word);
                 saveMark(sentence, element);
                 word.setLength(0);
-                continue;
             } else if (element == ' ' && word.length() == 0) {
                 saveMark(sentence, element);
-                continue;
-            }
-
-            if (PUNCTUATION_MARK.contains(String.valueOf(element)) && word.length() != 0) {
+            } else if (PUNCTUATION_MARK.contains(String.valueOf(element)) && word.length() != 0) {
                 saveWord(sentence, word);
                 saveMark(sentence, element);
                 word.setLength(0);
-                continue;
             } else if (PUNCTUATION_MARK.contains(String.valueOf(element)) && word.length() == 0) {
                 saveMark(sentence, element);
-                continue;
-            }
-            if (END_OF_SENTENCE_MARK.contains(String.valueOf(element)) && word.length() != 0 && sentenceInChar.length > counter) {
+            } else if (END_OF_SENTENCE_MARK.contains(String.valueOf(element)) && word.length() != 0 && sentenceInChar.length > counter) {
                 word.append(element);
-                continue;
-            } else if ((END_OF_SENTENCE_MARK.contains(String.valueOf(element)) && word.length()!=0 && sentenceInChar.length==counter)) {
+            } else if ((END_OF_SENTENCE_MARK.contains(String.valueOf(element)) && word.length() != 0 && sentenceInChar.length == counter)) {
                 saveWord(sentence, word);
                 saveMark(sentence, element);
-                word.setLength(0);
             }
+            previousElement = element;
         }
         return sentence;
     }
